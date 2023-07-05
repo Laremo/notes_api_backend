f3d1f698cfb6e9cfd88c43d6aa0dd5e103c06097
Laremo <memoo.lara12306@hotmail.com>
2023-07-04 20:13:18 -0600

    Finished login and token management services

--- middleware/checkLoggedUser.js
+++ middleware/checkLoggedUser.js
@@ -1,8 +1,35 @@
+const dbUsersService = require('../src/database/dbUsersService');
 const jwt = require('jsonwebtoken');
 
-const CheckLoggedUser = (req, res, next) => {
+const CheckLoggedUser = async (req, res, next) => {
+  //we only allow the requests to create a new user
+  if (req.baseUrl === '/api/users' && req.method === 'POST') {
+    return next();
+  }
   const auth = req.get('authorization');
-  if (!auth) return res.status(401).json({ msg: 'not logged in' });
+  let token = null;
+  if (!auth) return res.status(401).json({ error: 'not logged in' });
+
+  if (auth && auth.toLowerCase().startsWith('bearer')) {
+    token = auth.slice(auth.indexOf(' ') + 1);
+  }
+  let decodedToken = null;
+  try {
+    decodedToken = jwt.verify(token, process.env.SECRET);
+  } catch {
+    token = null;
+  }
+
+  if (!token || !decodedToken.id)
+    return res
+      .status(401)
+      .json({ token: null, error: 'Missing or invalid token' });
+
+  const exisitingUser = await dbUsersService.getUserByID(decodedToken.id);
+  if (!exisitingUser)
+    return res
+      .status(401)
+      .json({ token: null, error: 'Missing or invalid token' });
 
   return next();
 };
